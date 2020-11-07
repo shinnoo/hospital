9ad1e037d3ba5196ce8bf6f048fa102d8b3c574f
trandung <trandung164.ptit@gmail.com>
2020-11-07 11:30:13 +0700

    add sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,37 +1,11 @@
-create table if not exists Ingredient (
- id varchar(4) not null,
- name varchar(25) not null,
- type varchar(10) not null,
- PRIMARY KEY (id)
-);
-create table if not exists Taco (
- id int NOT NULL,
- name varchar(50) not null,
- createdAt timestamp not null,
- PRIMARY KEY (id)
-);
-create table if not exists Taco_Ingredients (
- taco int not null,
- ingredient varchar(4) not null,
- FOREIGN KEY (taco) REFERENCES Taco(id),
- FOREIGN KEY (ingredient) REFERENCES Ingredient(id)
-);
-create table if not exists Taco_Order (
-id int NOT NULL,
- deliveryName varchar(50) not null,
- deliveryStreet varchar(50) not null,
- deliveryCity varchar(50) not null,
- deliveryState varchar(2) not null,
- deliveryZip varchar(10) not null,
- ccNumber varchar(16) not null,
- ccExpiration varchar(5) not null,
- ccCVV varchar(3) not null,
- placedAt timestamp not null,
-PRIMARY KEY (id)
-);
-create table if not exists Taco_Order_Tacos (
- tacoOrder int not null,
- taco int not null,
- FOREIGN KEY (tacoOrder) REFERENCES Taco_Order(id),
- FOREIGN KEY (taco) REFERENCES Taco(id)
-);
+CREATE TABLE `doctor` (
+  `id` varchar(255) NOT NULL,
+  `id_card` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `level` varchar(255) DEFAULT NULL,
+  `experience` varchar(255) DEFAULT NULL,
+  `type` varchar(255) DEFAULT NULL,
+  `academic_level` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
